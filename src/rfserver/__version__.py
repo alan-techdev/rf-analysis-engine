0b872f84c95bfd55963f5a4ddc015f610a19591e
iso <iso.techdev@gmail.com>
2026-09-08 13:42:36 +0300

    Added parser in the main

---
+++ src/rfserver/__version__.py
@@ -0,0 +1,10 @@
+"""
+Application version
+Application Summary
+"""
+__title__ = "RFC Server"
+__description__ = "Radio Frequency Surveillance Server"
+__url__ = "https://rf-analysis-engine.readthedocs.io/en/latest/"
+__version__ = "1.0.2"
+__author__ = "Alan M - Iso H"
+__license__ = "AGPL-3.0-only"
--- src/rfserver/main.py
+++ src/rfserver/main.py
@@ -1,7 +1,106 @@
+from argparse import ArgumentParser, Namespace
 from rfserver.rest_api import rest
+
+from rfserver import (
+    __author__,  # type:ignore
+    __description__,  # type:ignore
+    __license__,  # type:ignore
+    __title__,  # type:ignore
+    __url__,  # type:ignore
+    __version__,  # type:ignore
+)
+from rfserver._help import bug_reporting
+
+"""
+A- Make the project in edit mode
+$ pwd
+/home/alan/workspace-python/RTL-SDR/rf-surveillance
+$ pip install -e .
+$ rfnode setting.json -vvv -ld /home/alan/tmp
+
+//////////////////////////////////////////
+
+B- Using PYTHONPATH (Not recommended)
+Linux:
+=====
+$ export PYTHONPATH=/home/alan/workspace-python/RTL-SDR/rf-surveillance/src
+$ pwd
+ /home/alan/workspace-python/RTL-SDR/rf-surveillance/src
+$ python rfnode setting.json -vvv -ld /home/alan/tmp
+
+Windows:
+========
+set PYTHONPATH=/home/alan/workspace-python/RTL-SDR/rf-surveillance/src
+echo %PYTHONPATH%
+python rfnode setting.json -vvv -ld /home/alan/tmp
+Note: Check the devicemanager from the control panel for the port name
+"""
 
 
 def main() -> None:
+    """
+    The main function is called only when the script is executed directly,
+    Execute data acquisition system using RTL-SDR devices.
+    It initializes a logging manager and configures it based on command-line arguments, responsible
+    for managing data flow between different components of the system.
+
+
+    """
+    # https://docs.python.org/3/howto/argparse.html
+    parser = ArgumentParser(
+        prog = "rfserver",
+        usage="rfserver [-h] [-ld dir] [-v] [--version] [--author] [--report-bug] [--description] [--license] [--title] [--url]",
+        description=" The server for Radio Frequency Scanner which capture given power threshold"
+    )
+
+    parser.add_argument(
+        "-ld",
+        "--log_directory",
+        help="store output log in a directory",
+        type=str,
+        metavar="dir",
+    )
+    parser.add_argument("-v",
+                         "--verbose", help="increase output verbosity.Default to Error if not supplied, 40 is Debug", type=int, metavar="", default=0)
+
+    parser.add_argument("--version", action="store_true", help="Display current library version")
+    parser.add_argument("--author", action="store_true", help="Display author information")
+    parser.add_argument("--report-bug", action="store_true", help="Library detail information to report a bug")
+    parser.add_argument("--description", action="store_true", help="Display description of the package")
+    parser.add_argument("--license", action="store_true", help="Display license information")
+    parser.add_argument("--title", action="store_true", help="Display title of the package")
+    parser.add_argument("--url", action="store_true", help="Display read the docs url of the package")
+
+    args: Namespace = parser.parse_args()
+
+    if args.version:
+        print(f"Version: {__version__}")
+        return
+
+    if args.author:
+        print(f"Author: {__author__}")
+        return
+
+    if args.report_bug:
+        bug_reporting()
+        return
+
+    if args.description:
+        print(f"Description: {__description__}")
+        return
+
+    if args.license:
+        print(f"License: {__license__}")
+        return
+
+    if args.title:
+        print(f"Title: {__title__}")
+        return
+
+    if args.url:
+        print(f"URL: {__url__}")
+        return
+    
     rest.app.run()
 
 
